9cf6e0d1566bc2fd754b60c918b9428465e92317
skotakri <syam.skr82@gmail.com>
2022-08-01 10:33:06 +0530

    test commit

---
+++ java_project/src/java_project/hellowworld.java
@@ -0,0 +1,10 @@
+package java_project;
+
+public class hellowworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("this is first java program...");
+	}
+
+}
